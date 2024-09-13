cae511e1204b8411f33a69585bdc091a1ec2692a
dikjain <dikshitmahanot2005@gmail.com>
2024-09-14 00:28:43 +0530

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,12 +46,12 @@ function App({username}) {
           <input type="text" value={text} onChange={(e)=>settext(e.target.value) & e.preventDefault()} id="msg" className='w-[80%] h-[80%] px-3 rounded-xl border-0 outline-none' placeholder='Enter your message...'/>
           <button onClick={handlesubmit} className='btn w-[10%] h-[80%] rounded-xl bg-green-500 text-white text-xl'>Send</button>
         </div>
-       <div id="onlineuser" className='w-[15%] h-fit bg-blue-400 p-3 text-[2.5vw] absolute left-[10%] top-[15%] rounded-xl'>
+       <div id="onlineuser" className='w-[15%] h-fit bg-blue-300 p-3 text-[2.5vw] absolute left-[10%] top-[15%] rounded-xl'>
          <h2 className='onu text-white  w-full h-fit bg-green-500 rounded-xl p-1'>Online Users</h2>
          {onlineuser.map((q,i)=>(<div key={i} className='py-2 px-3 w-full text-white '>{q.id == id ? `you ( ${q.name} )` : `${q.name}`}</div>))}
 
         </div> 
-      <div id="container" className='w-[40%] overflow-y-scroll h-[70%]   py-5  rounded-xl flex flex-col items-center justify-start bg-blue-300'>
+      <div id="container" className='w-[40%] overflow-y-scroll h-[70%] msgprnt py-5  rounded-xl flex flex-col items-center justify-start bg-blue-300'>
         {msgs.map((q,i)=>q.type == "message" ? (<div id="messagebox" key={i} className={` {min-w-[20%]  w-fit my-4 min-h-fit h-fit break-words mx-[10px] px-[10px] py-[5px] ${q.id == id ? "bg-green-500 self-start" : "bg-blue-500 self-end"} rounded-xl text-center max-w-[50%] text-white}`}><span className='font-extrabold my-4 font-mono text-white'>{q.name} :</span> {q.message}</div>) : (<div id="messagebox" key={i} className={`{min-w-[20%] min-h-[6%] bg-gray-300  mx-[10px] px-[10px] py-[5px] rounded-xl my-3 text-center max-w-[50%] text-white}`}>{q.name == username ? "you" : q.name} {q.message} The Chat</div>))}
         <div ref={messagesEndRef} /> 
       </div>
